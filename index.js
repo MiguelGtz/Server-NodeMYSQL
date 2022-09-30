9cf31cca0b07c09912ecabcadc911ef12e679b96
Miguel Gutiérrez <miguelagtz99@gmail.com>
2022-09-30 16:07:35 -0500

    localhost

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(json());
 app.use(function (_, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
